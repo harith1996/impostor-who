a69a595540605f3b9e7167929840a33ff395bde6
Harith Rathish <harith1996@gmail.com>
2025-11-08 20:33:06 +0100

    change shuffle method

--- utils/gameUtils.js
+++ utils/gameUtils.js
@@ -4,15 +4,30 @@ const create_game = (userIds) => {
         'state': 'waiting',
     };
 }
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
 const find_player_in_game = (game, member) => {
     return game.players.find(player => player.user.id === member.user.id);
 }
 const get_word_picker_and_impostor = (game) => {
     //pick two random players as the word picker and impostor
-    const shuffledPlayers = game.players.sort(() => 0.5 - Math.random());
+    shuffle(game.players); 
     return {
-        word_picker: shuffledPlayers[0],
-        impostor: shuffledPlayers[1],
+        word_picker: game.players[0],
+        impostor: game.players[1],
     };
 }
 const getRandomPlayer = (players) => {
