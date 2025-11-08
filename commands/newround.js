9692de2bb70abe9187c81df5693bf5cd52e6c781
Harith Rathish <harith1996@gmail.com>
2025-11-08 16:43:30 +0100

    notify innocents

--- commands/newround.js
+++ commands/newround.js
@@ -4,23 +4,26 @@ import { sendDM } from "../utils/messageUtils.js";
 import { wordPickerMessage, impostorMessage } from "../assets/messages.js";
 
 export default async function handleNewRoundCommand(activeGames, res) {
-    console.log('Handling newround command');
-    if (activeGames.length === 0) {
-        return respond(res, 'No active games. Please join a game first!');
+  console.log('Handling newround command');
+  if (activeGames.length === 0) {
+    return respond(res, 'No active games. Please join a game first!');
+  }
+  const game = activeGames[0]; // For simplicity, use the first active game
+  if (game.players.length < 2) {
+    return respond(res, 'Not enough players to start a new round. Please wait for more players to join!');
+  }
+  const { word_picker, impostor } = get_word_picker_and_impostor(game);
+  console.log(`New round: Word Picker - ${word_picker.nick}, Impostor - ${impostor.nick}`);
+  activeGames[0].currentRound = { word_picker, impostor };
+  await sendDM(word_picker.user.id, wordPickerMessage);
+  await sendDM(impostor.user.id, impostorMessage);
+
+  respond(res, `New round started! Wait for the word picker to choose a word! 👀`);
+  //send a DM to everyone else 
+  for (const player of game.players) {
+    if (player.user.id !== word_picker.user.id && player.user.id !== impostor.user.id) {
+      await sendDM(player.user.id, `You're innocent! Try to figure out who the impostor is this round.`);
     }
-    const game = activeGames[0]; // For simplicity, use the first active game
-    if (game.players.length < 2) {
-        return respond(res, 'Not enough players to start a new round. Please wait for more players to join!');
-    }
-    const { word_picker, impostor } = get_word_picker_and_impostor(game);
-    console.log(`New round: Word Picker - ${word_picker.nick}, Impostor - ${impostor.nick}`);
-    activeGames[0].currentRound = { word_picker, impostor };
-            try {
-              await sendDM(word_picker.user.id, wordPickerMessage);
-              await sendDM(impostor.user.id, impostorMessage);
-            } catch (err) {
-              console.error('Failed to send DMs to word picker and impostor:', err);
-            }
-    respond(res, `New round started! Wait for the word picker to choose a word! 👀`);
-    return { word_picker, impostor };
+  }
+  return { word_picker, impostor };
 }
